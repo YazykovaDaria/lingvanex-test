152a60843d749311b73623714f649f8e0344aac7
YazykovaDaria <yazykovadasva@gmail.com>
2024-04-19 11:24:20 +0300

    feat: setup project

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import 'the-new-css-reset/css/reset.css';
+import '../styles/style.css';
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+// hi
+//   </div>
+// `;
+
+// console.log('hi');
